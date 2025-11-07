dc1878c90b2cbcae52f0cff17830691a2a1c7648
Cawaa <binabibiaa@gmail.com>
2025-11-07 15:03:08 +0700

    addFile : PunyaSalwa.py

--- PunyaKetua.py
+++ PunyaKetua.py
@@ -1,1 +1,7 @@
 print("INI PUNYA KETUA")
+
+a = int(input("Masukan angka pertama: "))
+b = int(input("Masukan angka kedua: "))
+c = a*b
+
+print(f"Hasil perkalian: {c}")
---
+++ PunyaSalwa.py
@@ -0,0 +1,1 @@
+print("Ini punya Salwa, Halo!")
